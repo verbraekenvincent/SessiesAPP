7ea485b75a142b382d9bd375553b448b2f53802c
Vincent Verbraeken <verbraeken.vincent@gmail.com>
2016-06-06 20:29:34 +0200

    Added dbcontext

---
+++ SessiesAPP/DAL/SessionContext.cs
@@ -0,0 +1,24 @@
+﻿using SessiesAPP.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SessiesAPP.DAL
+{
+    public class SessionContext
+    {
+        public class SchoolContext : DbContext
+        {
+
+            public SchoolContext() : base("SchoolContext")
+            {
+            }
+
+            public DbSet<Session> Sessions { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            }
+        }
+    }
+}
---
+++ SessiesAPP/Models/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SessiesAPP.Models
+{
+    public class Session
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public int attendees { get; set; }
+    }
+}
